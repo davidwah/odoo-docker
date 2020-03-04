92f154de21d0653dbeed338998af931ff70664f7
David Wahyu <22112331+davidwah@users.noreply.github.com>
2020-03-04 15:15:42 +0700

    Create __manifest__.py

---
+++ my-addons/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name"          : "Create Addons",
+    "version"       : "1.0",
+    "author"        : "DWP",
+    "website"       : "dwp.my.id",
+    "category"      : "New Module",
+    "summary"       : "Coba buat addons baru",
+    "description"   : """
+        	Percobaan pertama membuat addons baru di Odoo
+    	""",
+    "depends"   : [
+        "base",
+    ],
+    "data"      : [
+
+    ],
+    "demo"      : [],
+    "test"      : [],
+    "images"    : [],
+    "qweb"      : [],
+    "css"       : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+}
